714ba66008188ddce4ccac152abd6dff8d564c59
Ben Wernicke <ben@wernicke.at>
2025-04-11 02:20:50 +0200

    more stuff

--- mittelmeer/src/migrations.rs
+++ mittelmeer/src/migrations.rs
@@ -6,25 +6,16 @@ impl Migration for Migration000 {
     fn up(&self, db: &rusqlite::Connection) -> Result<(), anyhow::Error> {
         db.execute(
             "
-            CREATE TABLE entry (
-                id          INTEGER NOT NULL UNIQUE,
-                title       TEXT NOT NULL,
-                last_update INTEGER NOT NULL,
+            CREATE TABLE todo (
+                id      INTEGER NOT NULL UNIQUE,
+                title   TEXT NOT NULL,
+                opened  INTEGER,
+                closed  INTEGER,
+
+                last_change INTEGER NOT NULL,
+                path        INTEGER NOT NULL,
 
                 PRIMARY KEY(id)
-            );
-            ",
-            rusqlite::params![],
-        )?;
-
-        db.execute(
-            "
-            CREATE TABLE todos (
-                entry       INTEGER NOT NULL UNIQUE,
-                opened      INTEGER,
-                closed      INTEGER NOT NULL,
-
-                FOREIGN KEY(entry) REFERENCES entry(id)
             );
             ",
             rusqlite::params![],
