8835a37d0f8c494938d54bda45b37a07913fe39e
Ben Wernicke <ben@wernicke.at>
2025-06-14 17:39:16 +0200

    json is now being printed

--- src/cli/todo.rs
+++ src/cli/todo.rs
@@ -1,6 +1,7 @@
 use crate::context::Context;
 use crate::table;
 use crate::todo::Todo;
+use serde_json;
 
 #[derive(Debug, clap::Args)]
 pub struct Args {
@@ -24,33 +25,7 @@ pub fn run(context: Context, args: Args) -> Result<(), anyhow::Error> {
     }
 }
 
-fn run_list(context: Context, _: ListArgs) -> Result<(), anyhow::Error> {
-    let todos: Result<Vec<Todo>, anyhow::Error> = context
-        .db
-        .prepare(
-            "
-            SELECT 
-                todos.title, 
-                todos.deadline, 
-                todos.scheduled,
-                todos.line,
-                files.path
-            FROM todos INNER JOIN files ON todos.file = files.id;",
-        )?
-        .query(rusqlite::params![])?
-        .and_then(|row| {
-            Ok(Todo {
-                title: row.get(0)?,
-                deadline: row.get(1)?,
-                scheduled: row.get(2)?,
-                line_number: row.get(3)?,
-                file: row.get::<_, String>(4)?.into(),
-            })
-        })
-        .collect();
-
-    let todos = todos?;
-
+fn list_todos_plain(todos: Vec<Todo>) -> Result<(), anyhow::Error> {
     let table = table::Row::new()
         .add_col("Title".into())
         .add_col("Scheduled".into())
@@ -83,4 +58,45 @@ fn run_list(context: Context, _: ListArgs) -> Result<(), anyhow::Error> {
 
     println!("{}", table);
     Ok(())
+}
+
+fn list_todos_json(todos: Vec<Todo>) -> Result<(), anyhow::Error> {
+    let output = serde_json::to_vec_pretty(&todos)?;
+    let output = String::from_utf8(output)?;
+    println!("{}", output);
+    Ok(())
+}
+
+fn run_list(context: Context, _: ListArgs) -> Result<(), anyhow::Error> {
+    let todos: Result<Vec<Todo>, anyhow::Error> = context
+        .db
+        .prepare(
+            "
+            SELECT 
+                todos.title, 
+                todos.deadline, 
+                todos.scheduled,
+                todos.line,
+                files.path
+            FROM todos INNER JOIN files ON todos.file = files.id;",
+        )?
+        .query(rusqlite::params![])?
+        .and_then(|row| {
+            Ok(Todo {
+                title: row.get(0)?,
+                deadline: row.get(1)?,
+                scheduled: row.get(2)?,
+                line_number: row.get(3)?,
+                file: row.get::<_, String>(4)?.into(),
+            })
+        })
+        .collect();
+
+    let todos = todos?;
+
+    match context.output_format {
+        crate::context::OutputFormat::Colorful => list_todos_plain(todos),
+        crate::context::OutputFormat::Plain => list_todos_plain(todos),
+        crate::context::OutputFormat::Json => list_todos_json(todos),
+    }
 }
